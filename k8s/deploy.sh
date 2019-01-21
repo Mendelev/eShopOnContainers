d829978c2e78b7116bd32da57a7516c500fee007
Yuri Santana <yurisantana@poli.ufrj.br>
2019-01-21 11:06:16 -0200

    Update deploy.sh - Teste com Ingress

--- k8s/deploy.sh
+++ k8s/deploy.sh
@@ -59,6 +59,10 @@ while [[ $# -gt 0 ]]; do
         container_registry="$2"; shift 2 ;;
     -t | --tag )
         image_tag="$2"; shift 2 ;;
+    #-u | --user )
+    #    docker_user="$2"; shift 2 ;;
+    #-p | --user )
+    #    docker_password="$2"; shift 2 ;;
     -b | --build-solution )
         build_solution='yes'; shift ;;
     --skip-image-build )
@@ -73,12 +77,36 @@ while [[ $# -gt 0 ]]; do
   esac
 done
 
+echo "#################### NGINX ####################"
+externalDns=$(kubectl get svc ingress-nginx -n ingress-nginx -o=jsonpath="{.status.loadBalancer.ingress[0].ip}")
+if [[!$externalDns]]; then
+     echo "Não possui nginx-ingress rodando, aplicando yaml...
+     kubectl apply -f nginx-ingress/mandatory.yaml
+     kubectl apply -f nginx-ingress/service-nodeport.yaml
+     echo "Verifique se o nginx ingress está gerando ip e então rode de novo o deploy.sh"
+     exit 3
+fi
+
 if [[ ! $container_registry ]]; then
     echo 'Container registry must be specified (e.g. myregistry.azurecr.io)'
     echo ''
     usage
     exit 3
 fi
+
+#if [[ ! $docker_user ]]; then
+#    echo 'Registry docker user must be specified (e.g. myregistry.azurecr.io)'
+#    echo ''
+#    usage
+#    exit 3
+#fi
+
+#if [[ ! $docker_password ]]; then
+#    echo 'Registry docker password must be specified (e.g. myregistry.azurecr.io)'
+#    echo ''
+#    usage
+#    exit 3
+#fi
 
 if [[ $build_solution ]]; then
     echo "#################### Building eShopOnContainers solution ####################"
@@ -107,78 +135,60 @@ if [[ $push_images ]]; then
     done
 fi
 
+echo "#################### Loggin on resgitry ####################"
+
+#docker login -u mendelev -p cp9yzw12
+#kubectl delete secret docker-registry regcred
+#kubectl create secret docker-registry regcred --docker-server=https://registry-1.docker.io/v2/ --docker-username=root --docker-password=Inovax123! --docker-email=yuricamargo4@gmail.com
+
 echo "#################### Cleaning up old deployment ####################"
 kubectl delete deployments --all
 kubectl delete services --all
-kubectl delete configmap config-files || true
+kubectl delete configmap internalurls || true
 kubectl delete configmap urls || true
 kubectl delete configmap externalcfg || true
+kubectl delete configmap ocelot || true
+#kubectl delete -f ingress.yaml
 
-echo "#################### Deploying infrastructure components ####################"
-kubectl create configmap config-files --from-file=nginx-conf=nginx.conf
-kubectl label configmap config-files app=eshop
+echo "#################### Deploying infrastructure components ####################" 
 kubectl create -f sql-data.yaml -f basket-data.yaml -f keystore-data.yaml -f rabbitmq.yaml -f nosql-data.yaml
+echo "#################### Deploying ocelot APIGW ####################" 
+kubectl create configmap ocelot --from-file=mm=ocelot/configuration-mobile-marketing.json --from-file=ms=ocelot/configuration-mobile-shopping.json --from-file=wm=ocelot/configuration-web-marketing.json --from-file=ws=ocelot/configuration-web-shopping.json
+kubectl apply -f ocelot/deployment.yaml
+kubectl apply -f ocelot/service.yaml
 
 echo "#################### Creating application service definitions ####################"
-kubectl create -f services.yaml -f frontend.yaml
+kubectl create -f services.yaml
 
-echo "#################### Waiting for Azure to provision external IP ####################"
-
-ip_regex='([0-9]{1,3}\.){3}[0-9]{1,3}'
-while true; do
-    printf "."
-    frontendUrl=$(kubectl get svc frontend -o=jsonpath="{.status.loadBalancer.ingress[0].ip}")
-    if [[ $frontendUrl =~ $ip_regex ]]; then
-        break
-    fi
-    sleep 5s
-done
-
+#externalDns="localhost"
 printf "\n"
-externalDns=$frontendUrl
 echo "Using $externalDns as the external DNS/IP of the K8s cluster"
 
 echo "#################### Creating application configuration ####################"
 
 # urls configmap
+
+kubectl create -f internalurls.yaml
 kubectl create configmap urls \
-    "--from-literal=BasketUrl=http://basket" \
-    "--from-literal=BasketHealthCheckUrl=http://basket/hc" \
-    "--from-literal=CatalogUrl=http://$externalDns/catalog-api" \
-    "--from-literal=CatalogHealthCheckUrl=http://catalog/hc" \
-    "--from-literal=PicBaseUrl=http://$externalDns/catalog-api/api/v1/catalog/items/[0]/pic/" \
-    "--from-literal=Marketing_PicBaseUrl=http://$externalDns/marketing-api/api/v1/campaigns/[0]/pic/" \
-    "--from-literal=IdentityUrl=http://$externalDns/identity" \
-    "--from-literal=IdentityHealthCheckUrl=http://identity/hc" \
-    "--from-literal=OrderingUrl=http://ordering" \
-    "--from-literal=OrderingHealthCheckUrl=http://ordering/hc" \
-    "--from-literal=MvcClientExternalUrl=http://$externalDns/webmvc" \
-    "--from-literal=WebMvcHealthCheckUrl=http://webmvc/hc" \
-    "--from-literal=MvcClientOrderingUrl=http://ordering" \
-    "--from-literal=MvcClientCatalogUrl=http://catalog" \
-    "--from-literal=MvcClientBasketUrl=http://basket" \
-    "--from-literal=MvcClientMarketingUrl=http://marketing" \
-    "--from-literal=MvcClientLocationsUrl=http://locations" \
-    "--from-literal=MarketingHealthCheckUrl=http://marketing/hc" \
-    "--from-literal=WebSpaHealthCheckUrl=http://webspa/hc" \
-    "--from-literal=SpaClientMarketingExternalUrl=http://$externalDns/marketing-api" \
-    "--from-literal=SpaClientOrderingExternalUrl=http://$externalDns/ordering-api" \
-    "--from-literal=SpaClientCatalogExternalUrl=http://$externalDns/catalog-api" \
-    "--from-literal=SpaClientBasketExternalUrl=http://$externalDns/basket-api" \
-    "--from-literal=SpaClientIdentityExternalUrl=http://$externalDns/identity" \
-    "--from-literal=SpaClientLocationsUrl=http://$externalDns/locations-api" \
-    "--from-literal=LocationsHealthCheckUrl=http://locations/hc" \
-    "--from-literal=SpaClientExternalUrl=http://$externalDns" \
-    "--from-literal=LocationApiClient=http://$externalDns/locations-api" \
-    "--from-literal=MarketingApiClient=http://$externalDns/marketing-api" \
-    "--from-literal=BasketApiClient=http://$externalDns/basket-api" \
-    "--from-literal=OrderingApiClient=http://$externalDns/ordering-api" \
-    "--from-literal=PaymentHealthCheckUrl=http://payment/hc"
+    "--from-literal=PicBaseUrl=http://$externalDns/webshoppingapigw/api/v1/c/catalog/items/[0]/pic/" \
+    "--from-literal=Marketing_PicBaseUrl=http://$externalDns/webmarketingapigw/api/v1/m/campaigns/[0]/pic/" \
+    "--from-literal=mvc_e=http://$externalDns/webmvc" \
+    "--from-literal=marketingapigw_e=http://$externalDns/webmarketingapigw" \
+    "--from-literal=webshoppingapigw_e=http://$externalDns/webshoppingapigw" \
+    "--from-literal=mobileshoppingagg_e=http://$externalDns/mobileshoppingagg" \
+    "--from-literal=webshoppingagg_e=http://$externalDns/webshoppingagg" \
+    "--from-literal=identity_e=http://$externalDns/identity" \
+    "--from-literal=spa_e=http://$externalDns" \
+    "--from-literal=locations_e=http://$externalDns/locations-api" \
+    "--from-literal=marketing_e=http://$externalDns/marketing-api" \
+    "--from-literal=basket_e=http://$externalDns/basket-api" \
+    "--from-literal=ordering_e=http://$externalDns/ordering-api" \
+    "--from-literal=xamarin_callback_e=http://$externalDns/xamarincallback"
 
 kubectl label configmap urls app=eshop
 
 # externalcfg configmap -- points to local infrastructure components (rabbitmq, SQL Server etc)
-kubectl create -f conf_local.yml
+kubectl create -f conf_local.yaml
 
 # Create application pod deployments
 kubectl create -f deployments.yaml
@@ -186,16 +196,36 @@ kubectl create -f deployments.yaml
 echo "#################### Deploying application pods ####################"
 
 # update deployments with the correct image (with tag and/or registry)
-kubectl set image deployments/basket "basket=$container_registry/basket.api:$image_tag"
-kubectl set image deployments/catalog "catalog=$container_registry/catalog.api:$image_tag"
-kubectl set image deployments/identity "identity=$container_registry/identity.api:$image_tag"
-kubectl set image deployments/ordering "ordering=$container_registry/ordering.api:$image_tag"
-kubectl set image deployments/marketing "marketing=$container_registry/marketing.api:$image_tag"
-kubectl set image deployments/locations "locations=$container_registry/locations.api:$image_tag"
-kubectl set image deployments/payment "payment=$container_registry/payment.api:$image_tag"
-kubectl set image deployments/webmvc "webmvc=$container_registry/webmvc:$image_tag"
-kubectl set image deployments/webstatus "webstatus=$container_registry/webstatus:$image_tag"
-kubectl set image deployments/webspa "webspa=$container_registry/webspa:$image_tag"
+#kubectl set image deployments/basket "basket=$container_registry/basket.api:$image_tag"
+#kubectl set image deployments/catalog "catalog=$container_registry/catalog.api:$image_tag"
+#kubectl set image deployments/identity "identity=$container_registry/identity.api:$image_tag"
+#kubectl set image deployments/ordering "ordering=$container_registry/ordering.api:$image_tag"
+#kubectl set image deployments/marketing "marketing=$container_registry/marketing.api:$image_tag"
+#kubectl set image deployments/locations "locations=$container_registry/locations.api:$image_tag"
+#kubectl set image deployments/payment "payment=$container_registry/payment.api:$image_tag"
+#kubectl set image deployments/webmvc "webmvc=$container_registry/webmvc:$image_tag"
+#kubectl set image deployments/webstatus "webstatus=$container_registry/webstatus:$image_tag"
+#kubectl set image deployments/webspa "webspa=$container_registry/webspa:$image_tag"
+
+kubectl set image deployments/basket "basket=$container_registry:basket.api"
+kubectl set image deployments/catalog "catalog=$container_registry:catalog.api"
+kubectl set image deployments/identity "identity=$container_registry:identity.api"
+kubectl set image deployments/ordering "ordering=$container_registry:ordering.api"
+kubectl set image deployments/marketing "marketing=$container_registry:marketing.api"
+kubectl set image deployments/locations "locations=$container_registry:locations.api"
+kubectl set image deployments/payment "payment=$container_registry:payment.api"
+kubectl set image deployments/webmvc "webmvc=$container_registry:webmvc"
+kubectl set image deployments/webstatus "webstatus=$container_registry:webstatus"
+kubectl set image deployments/webspa "webspa=$container_registry:webspa"
+kubectl set image deployments/ordering-signalrhub "ordering-signalrhub=$container_registry:ordering-signalrhub"
+#kubectl set image deployments/ordering-signalrhub "ordering-signalrhub=$container_registry/ordering.signalrhub:$imageTag"
+kubectl set image deployments/mobileshoppingagg "mobileshoppingagg=$container_registry:mobileshoppingagg"
+kubectl set image deployments/webshoppingagg "webshoppingagg=$container_registry:webshoppingagg"
+
+kubectl set image deployments/apigwmm "apigwmm=$container_registry:ocelotapigw"
+kubectl set image deployments/apigwms "apigwms=$container_registry:ocelotapigw"
+kubectl set image deployments/apigwwm "apigwwm=$container_registry:ocelotapigw"
+kubectl set image deployments/apigwws "apigwws=$container_registry:ocelotapigw"
 
 kubectl rollout resume deployments/basket
 kubectl rollout resume deployments/catalog
@@ -207,6 +237,13 @@ kubectl rollout resume deployments/payment
 kubectl rollout resume deployments/webmvc
 kubectl rollout resume deployments/webstatus
 kubectl rollout resume deployments/webspa
+kubectl rollout resume deployments/mobileshoppingagg
+kubectl rollout resume deployments/webshoppingagg
+kubectl rollout resume deployments/apigwmm
+kubectl rollout resume deployments/apigwms
+kubectl rollout resume deployments/apigwwm
+kubectl rollout resume deployments/apigwws
+kubectl rollout resume deployments/ordering-signalrhub
 
 echo "WebSPA is exposed at http://$externalDns, WebMVC at http://$externalDns/webmvc, WebStatus at http://$externalDns/webstatus"
 echo "eShopOnContainers deployment is DONE"
